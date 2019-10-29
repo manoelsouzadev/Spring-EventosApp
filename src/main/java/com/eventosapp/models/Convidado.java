c9b5938bf8d056092b0cccd952857ad92394a23a
manoelsouzadev <manoel.souza280@gmail.com>
2019-10-29 15:44:28 -0300

    Criado novo controller, metodo no repository, model e views

---
+++ src/main/java/com/eventosapp/models/Convidado.java
@@ -0,0 +1,35 @@
+package com.eventosapp.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Convidado {
+	
+	@Id
+	private String rg;
+	private String nomeConvidado;
+	
+	@ManyToOne
+	private Evento evento;
+	
+	public String getNomeConvidado() {
+		return nomeConvidado;
+	}
+	public void setNomeConvidado(String nomeConvidado) {
+		this.nomeConvidado = nomeConvidado;
+	}
+	public String getRg() {
+		return rg;
+	}
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+	public Evento getEvento() {
+		return evento;
+	}
+	public void setEvento(Evento evento) {
+		this.evento = evento;
+	}
+}
