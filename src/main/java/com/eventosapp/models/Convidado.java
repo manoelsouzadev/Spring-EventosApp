c9ca372a3c66fa805474b67309cd573d7dd71a93
manoelsouzadev <manoel.souza280@gmail.com>
2019-11-18 20:12:23 -0300

    Adicionado validacoes nos campos dos formularios

--- src/main/java/com/eventosapp/models/Convidado.java
+++ src/main/java/com/eventosapp/models/Convidado.java
@@ -3,12 +3,16 @@ package com.eventosapp.models;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 public class Convidado {
 	
 	@Id
+	@NotEmpty
 	private String rg;
+	
+	@NotEmpty
 	private String nomeConvidado;
 	
 	@ManyToOne
